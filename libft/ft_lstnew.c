67a71737fb1444516be0b9d223b9346a2ec8749f
pingpanu <dingpanuwong@gmail.com>
2023-02-06 15:15:20 +0700

    adjust lstnew to this project

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pingpanu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 16:38:51 by pingpanu          #+#    #+#             */
-/*   Updated: 2022/03/04 16:40:31 by pingpanu         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:07:35 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
+
+static t_token newtoken(char *substr)
+{
+    if (strncmp(substr, ">", 2) == 0)
+        return (great);
+    else if (strncmp(substr, "<", 2) == 0)
+        return (less);
+    else if (strncmp(substr, ">>", 3) == 0)
+        return (greatgreat);
+    else if (strncmp(substr, ">&", 3) == 0)
+        return (greatamp);
+    else if (strncmp(substr, "|", 2) == 0)
+        return (piped);
+    else if (strncmp(substr, "&", 2) == 0)
+        return (ampersand);
+    else
+        return (word);
+}
 
 t_list	*ft_lstnew(void *content)
 {
@@ -20,6 +39,7 @@ t_list	*ft_lstnew(void *content)
 	if (!new)
 		return (NULL);
 	new->content = (void *)content;
+	new->token = newtoken((char *)content);
 	new->next = NULL;
 	return (new);
 }
