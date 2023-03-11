263d6524bbe1ef6e4bc5a6bec31e8cb152ddd20c
Pongpan Ingpanuwong <pingpanu@z2t16c4.42bangkok.com>
2023-03-11 15:07:54 +0700

    add sigaction

--- srcs/myshell_main.c
+++ srcs/myshell_main.c
@@ -1,27 +1,23 @@
 #include "myshell.h"
 
-/*void    sighandler(int signal)
+void    sighandler(int signal)
 {
-    if (signal == SIGINT)
-        printf("%p\n", NULL);
-    else if (signal == SIGQUIT)
-        return ;
-    else if (signal == EOF)
-        exit(0) ;
-}*/
+    if (signal == SIGINT) 
+        ;
+}
 
 int main(void)
 {
     char    *cmd_str = NULL;
     t_list  *cmd_ll = NULL;
-    //struct sigaction    act;
+    struct sigaction    act;
 
     //signal related function
-    /*ft_memset(&act, 0, sizeof(act));
-    act.sa_handler = &sighandler;
+    act.sa_handler = sighandler;
     act.sa_flags = 0;
     sigemptyset(&act.sa_mask);
-    sigaction(SIGINT, &act, NULL);*/
+    sigaction (SIGINT, &act, NULL);
+    sigaction (SIGTSTP, &act, NULL);
     //UI to be included here
     my_env.env_path = ft_split(getenv("PATH"), ':');
     //printf("%s\n", my_env.env_path);
@@ -29,6 +25,8 @@ int main(void)
     while (1) 
     {
         cmd_str = readline(my_env.dis_str);
+        if (!ft_strncmp(cmd_str, "", 1))
+            rl_redisplay();
         /*some function to receive cmd_str and execute it
         if (cmd_str != NULL) {
             lexer(&cmd_ll, cmd_str);
