e38943ea10da2aa0b079cb115383b78be02d3568
Pongpan Ingpanuwong <pingpanu@z2t7c1.42bangkok.com>
2023-03-15 15:57:53 +0700

    parser should support pipe

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,6 +1,6 @@
 #include "myshell.h"
 
-char    **ll_to_argv(t_list *cmd_ll)
+char    **ll_to_strarr(t_list *cmd_ll)
 {
     char    **argv;
     int     i = 0;
@@ -17,16 +17,99 @@ char    **ll_to_argv(t_list *cmd_ll)
     return (argv);
 }
 
-char    **parser(t_list *cmd_ll)
+void    print_ll(t_list *temp)
+{
+    t_list  *ptr;
+
+    ptr = temp;
+    while (ptr)
+    {
+        printf("%s -> ", ptr->token);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+t_list  *sub_linklist(t_list *left, t_list *right)
+{
+    t_list  *ret;
+
+    ret = NULL;
+    while(left && ft_strncmp(left->token, right->token, ft_strlen(right->token)) != 0)
+    {
+        ft_lstadd_back(&ret, ft_lstnew(left->token));
+        left = left->next;
+    }
+    return (ret);
+}
+
+t_cmd_node  *newnode(t_list *temp)
+{
+    t_cmd_node  *new;
+
+    new = malloc(sizeof(t_cmd_node));
+    if (!new || !temp)
+        return (NULL);
+    new->cmd_arr = ll_to_strarr(temp);
+    new->next = NULL;
+    return (new);
+}
+
+t_cmd_node  *lastnode(t_cmd_node *nodes)
+{
+    if (nodes == NULL)
+        return (NULL);
+    while (nodes->next != NULL)
+        nodes = nodes->next;
+    return (nodes);
+}
+
+void    node_addback(t_cmd_node *node, t_cmd_node *new)
+{
+    t_cmd_node  *tmp_nd;
+
+    if (!new)
+        return ;
+    if (node == NULL)
+        node = new;
+    else
+    {
+        tmp_nd = lastnode(node);
+        tmp_nd->next = new;
+    }
+}
+
+t_cmd_table parser(t_list *cmd_ll)
 {
     /*1) Check whether the next token is redirection*/
     /*2) Check whether the previous token is redirection*/
     /*3) Check if any prefix existed to activate expander*/
     /*4) Make command table by change the specific token to command table*/
-    char    **argv_table; //to store cmd_ll as argv table, so it's compatible with my executor
+    t_cmd_table     cmd_table; //to store cmd_ll as argv table, so it's compatible with my executor
+    t_list          *left_ptr;
+    t_list          *right_ptr;
+    t_list          *temp;
 
-    argv_table = ll_to_argv(cmd_ll);
-    if (!argv_table)
-        return (NULL);
-    return (argv_table);
+    //cmd_table = malloc(sizeof(t_cmd_table));
+    cmd_table.cmds = NULL;
+    cmd_table.infile = NULL;
+    cmd_table.outfile = NULL;
+    left_ptr = cmd_ll;
+    right_ptr = cmd_ll;
+    while (right_ptr)
+    {
+        /*parse cmd when pipe*/
+        if (isword(left_ptr) != 0)
+            left_ptr = left_ptr->next;
+        if (ft_strncmp(right_ptr->token, "PIPE", 5) == 0)
+        {
+            temp = sub_linklist(left_ptr, right_ptr);
+            print_ll(temp);
+            node_addback(cmd_table.cmds, newnode(temp));
+            ft_lstclear(&temp);
+            left_ptr = right_ptr;
+        }
+        right_ptr = right_ptr->next;
+    }
+    return (cmd_table);
 }
