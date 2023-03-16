5abfc2a65f3624581c8c01a56b9a63aadc8bdb32
Pongpan Ingpanuwong <pingpanu@z3t5c4.42bangkok.com>
2023-03-16 18:43:29 +0700

    support pipe

--- include/myshell.h
+++ include/myshell.h
@@ -2,9 +2,9 @@
 # define MYSHELL_H
 # define _XOPEN_SOURCE 700
 # include "libft.h" //already have unistd.h and stdlib.h
-# include <stdio.h> //printf
 # include <readline/readline.h> //readline, rl_*, and add_history
 # include <readline/history.h> //readline, rl_*, and add_history
+# include <stdio.h> //printf
 # include <fcntl.h> //open, unlink, dup, dup2, 
 # include <sys/types.h> //fork
 # include <sys/wait.h> //wait, waitpid, wait3, wait4
@@ -13,8 +13,13 @@
 # include <signal.h> //signal, sigaction, sigemptyset, sigaddset, and kill
 # include <dirent.h> //opendir, readdir, and closedir
 # include <errno.h> //stderror, and perror
-# include <termio.h> //support ioctl
+# ifndef APPLEß
+#  include <term.h>
+# else
+#  include <termio.h>
+# endif
 # include <termios.h> //tc*
+
 
 /*This is struct to store the computer systems*/
 typedef struct s_system 
@@ -23,16 +28,22 @@ typedef struct s_system
     char    *dis_str; //User displayed at readline function
 }   t_system;
 
-/*This is struct to get parameter for cmd_ll*/
-typedef struct s_cmd_param
+/*This is command node to store all cmds*/
+typedef struct s_cmd_node
 {
-    int     is_pipe; //check if there is '|'
-    int     is_amp; //check if there is '&'
-    int     is_gg; //check if there is '>>'
-    int     is_ga; //check if there is '>&'
-}   t_cmd_param;
+    char    **cmd_arr;
+    struct s_cmd_node   *next;
+}   t_cmd_node;
 
-void    lexer(t_list **cmd_ll, char* str);
-char    **parser(t_list *cmd_ll);
-int     executor(t_system my_env, char **cmd);
+/*This is struct to get parameter for cmd_ll*/
+typedef struct s_cmd_table
+{
+    t_cmd_node  *cmds;
+    char        *infile;
+    char        *outfile;        
+}   t_cmd_table;
+
+void        lexer(t_list **cmd_ll, char* str);
+t_cmd_table parser(t_list *cmd_ll);
+int         executor(t_system my_env, char **cmd);
 #endif
