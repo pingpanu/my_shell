983a75037ed62da52ea1ffdcb8c6e71c62752c92
pingpanu <dingpanuwong@gmail.com>
2023-03-29 10:39:07 +0700

    only quote removal

--- srcs/expander.c
+++ srcs/expander.c
@@ -1,2 +1,43 @@
 #include "myshell.h"
 
+static char *rem_str(char *str)
+{
+    char    *ret;
+    int     i = 0;
+    int     j = 0;
+    int     len = ft_strlen(str) - 2;
+
+    ret = ft_calloc(sizeof(char), len);
+    while (str[++i] && j < len)
+    {
+        ret[j] = str[i];
+        j++;
+    }
+    return (ret);
+}
+
+static void quote_remove(char **cmd_arr)
+{
+    int     i = 0;
+    
+    while (cmd_arr[i])
+    {
+        if (cmd_arr[i][0] == 39 || cmd_arr[i][0] == 34)
+            cmd_arr[i] = rem_str(cmd_arr[i]);
+        i++;
+    }
+}
+
+void    expander(t_cmd_table *cmdt)
+{
+    t_cmd_node  *ptr;
+
+    if (!cmdt->cmds)
+        return ;
+    ptr = cmdt->cmds;
+    while (ptr)
+    {
+        quote_remove(ptr->cmd_arr);
+        ptr = ptr->next;
+    }
+}
